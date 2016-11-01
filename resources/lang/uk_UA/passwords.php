d296a05a88877e05b7c1bf56744e9b7e80b7ce5d
beta37 <mikolay.korniat@gmail.com>
2016-11-01 15:16:54 +0200

    Translated

--- resources/lang/uk_UA/passwords.php
+++ resources/lang/uk_UA/passwords.php
@@ -10,10 +10,10 @@
  */
 
 return [
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'user'     => 'We can\'t find a user with that e-mail address.',
-    'token'    => 'This password reset token is invalid.',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'reset'    => 'Your password has been reset!',
-    'blocked'  => 'Nice try though.',
+    'password' => 'Пароль повинен містити не менше шести символів і збігатися з підтвердженням.',
+    'user'     => 'Ми не можемо знайти користувача з такою адресою електронної пошти.',
+    'token'    => 'Цей пароль скинути маркер є недійсним.',
+    'sent'     => 'Ми по електронній пошті, посилання на відновлення пароля!',
+    'reset'    => 'Ваш пароль скинутий!',
+    'blocked'  => 'Приємно, хоча спробувати.',
 ];
