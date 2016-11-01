b2c4bdd4e8db9ccb440fb4ecc32837e4c372b9a9
beta37 <mikolay.korniat@gmail.com>
2016-11-01 15:13:36 +0200

    New translations

--- resources/lang/es_ES/auth.php
+++ resources/lang/es_ES/auth.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'failed'   => '',
-    'throttle' => '',
+    'failed'   => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
