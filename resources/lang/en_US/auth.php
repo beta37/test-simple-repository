e61080c9c0792c2dfe1c8f0fe7952d52a8d5f28d
beta37 <mikolay.korniat@gmail.com>
2016-11-01 15:25:02 +0200

    Update auth.php

--- resources/lang/en_US/auth.php
+++ resources/lang/en_US/auth.php
@@ -24,5 +24,6 @@ return [
 
     'failed'   => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'testing field' => 'testing field'.
 
 ];
