ee5dc0cd29baca860fd727897ebf43c9c1d1011f
beta37 <mikolay.korniat@gmail.com>
2017-02-02 10:46:13 +0200

    New translations

---
+++ resources/lang/it_IT/help.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * help.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+
+    // tour!
+    'main-content-title'     => '',
+    'main-content-text'      => '',
+    'sidebar-toggle-title'   => '',
+    'sidebar-toggle-text'    => '',
+    'account-menu-title'     => '',
+    'account-menu-text'      => '',
+    'budget-menu-title'      => '',
+    'budget-menu-text'       => '',
+    'report-menu-title'      => '',
+    'report-menu-text'       => '',
+    'transaction-menu-title' => '',
+    'transaction-menu-text'  => '',
+    'option-menu-title'      => '',
+    'option-menu-text'       => '',
+    'main-content-end-title' => '',
+    'main-content-end-text'  => '',
+    'index'                  => '',
+    'home'                   => '',
+];
