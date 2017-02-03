bf6212e8887c6abfaf132e3566ccf4a7d42a7c79
beta37 <mikolay.korniat@gmail.com>
2017-02-03 11:17:41 +0200

    New translations

---
+++ resources/lang/uk_UA/config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * config.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+    'locale'          => '',
+    'month'           => '',
+    'month_and_day'   => '',
+    'date_time'       => '',
+    'specific_day'    => '',
+    'week_in_year'    => '',
+    'quarter_of_year' => '',
+    'year'            => '',
+    'half_year'       => '',
+
+];
