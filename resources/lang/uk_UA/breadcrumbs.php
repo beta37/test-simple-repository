3a990aa01cd4d3ff347cd37ff5a6ce5c48449081
beta37 <mikolay.korniat@gmail.com>
2017-02-03 11:17:38 +0200

    New translations

---
+++ resources/lang/uk_UA/breadcrumbs.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * breadcrumbs.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+    'home'              => '',
+    'edit_currency'     => '',
+    'delete_currency'   => '',
+    'newPiggyBank'      => '',
+    'edit_piggyBank'    => '',
+    'preferences'       => '',
+    'profile'           => '',
+    'changePassword'    => '',
+    'bills'             => '',
+    'newBill'           => '',
+    'edit_bill'         => '',
+    'delete_bill'       => '',
+    'reports'           => '',
+    'searchResult'      => '',
+    'withdrawal_list'   => '',
+    'deposit_list'      => '',
+    'transfer_list'     => '',
+    'transfers_list'    => '',
+    'create_withdrawal' => '',
+    'create_deposit'    => '',
+    'create_transfer'   => '',
+    'edit_journal'      => '',
+    'delete_journal'    => '',
+    'tags'              => '',
+    'createTag'         => '',
+    'edit_tag'          => '',
+    'delete_tag'        => '',
+];
