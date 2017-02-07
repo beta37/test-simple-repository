16d67fd87faa3b0b6bd3ab80b12b7fb693242f7d
beta37 <mikolay.korniat@gmail.com>
2017-02-07 08:12:22 +0200

    Translated

---
+++ resources/lang/it_IT/csv.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * csv.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+declare(strict_types = 1);
+
+return [
+
+    'import_configure_title' => '',
+    'import_configure_intro' => '',
+    'import_configure_form'  => '',
+    'header_help'            => '',
+    'date_help'              => '',
+    'delimiter_help'         => '',
+    'import_account_help'    => '',
+    'upload_not_writeable'   => '',
+
+    // roles
+    'column_roles_title'     => '',
+    'column_roles_text'      => '',
+    'column_roles_table'     => '',
+    'column_name'            => '',
+    'column_example'         => '',
+    'column_role'            => '',
+    'do_map_value'           => '',
+    'column'                 => '',
+    'no_example_data'        => '',
+    'store_column_roles'     => '',
+    'do_not_map'             => '',
+    'map_title'              => '',
+    'map_text'               => '',
+
+    'field_value'          => '',
+    'field_mapped_to'      => '',
+    'store_column_mapping' => '',
+
+    // map things.
+
+
+    'column__ignore'                => '',
+    'column_account-iban'           => '',
+    'column_account-id'             => '',
+    'column_account-name'           => '',
+    'column_amount'                 => '',
+    'column_amount-comma-separated' => '',
+    'column_bill-id'                => '',
+    'column_bill-name'              => '',
+    'column_budget-id'              => '',
+    'column_budget-name'            => '',
+    'column_category-id'            => '',
+    'column_category-name'          => '',
+    'column_currency-code'          => '',
+    'column_currency-id'            => '',
+    'column_currency-name'          => '',
+    'column_currency-symbol'        => '',
+    'column_date-interest'          => '',
+    'column_date-book'              => '',
+    'column_date-process'           => '',
+    'column_date-transaction'       => '',
+    'column_description'            => '',
+    'column_opposing-iban'          => '',
+    'column_opposing-id'            => '',
+    'column_external-id'            => '',
+    'column_opposing-name'          => '',
+    'column_rabo-debet-credit'      => '',
+    'column_ing-debet-credit'       => '',
+    'column_sepa-ct-id'             => '',
+    'column_sepa-ct-op'             => '',
+    'column_sepa-db'                => '',
+    'column_tags-comma'             => '',
+    'column_tags-space'             => '',
+    'column_account-number'         => '',
+    'column_opposing-number'        => '',
+];
